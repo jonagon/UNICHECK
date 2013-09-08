5da626dd38759c00164678770313a27c13cd6484
Jonas Bayer <jonagon@me.com>
2013-09-08 12:37:35 +0200

    2. update

--- Jonas/map/map.js
+++
@@ -1,4 +0,0 @@
-		var map = L.map('map').setView([51.505, -0.09], 13);
-		L.tileLayer('http://{s}.tile.cloudmade.com/5F9A940B68534DF98F4EC7F3C5B6C300/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',		maxZoom: 18
-		}).addTo(map);
